323f1f6eabfeddaebe48c009a9eba6e9dfe3c5f1
dghnts <mittsulann73@gmail.com>
2022-06-30 10:56:23 +0900

    update install.sh

---
+++ dotfiles-main/install.sh
@@ -0,0 +1,37 @@
+if [ ! -e ~/dotfiles ]; then
+	mkdir ~/dotfiles
+fi
+
+DOTPATH="dotfiles"
+
+if type git > /dev/null 2>&1; then
+	git clone --recursive "https://github.com/dghnts/dotfiles.git" "$DOTPATH"
+elif type curl > /dev/null 2>&1 || type wget > /dev/null 2>&1; then
+
+    tarball="https://github.com/dghnts/dotfiles/archive/main.tar.gz"
+
+    # どっちかでダウンロードして，tar に流す
+    if type curl > /dev/null 2>&1; then
+        curl -L "$tarball"
+
+    elif type wget > /dev/null/ 2>&1; then
+        wget -O - "$tarball"
+	
+    fi | tar zxv
+
+    # 解凍したら，DOTPATH に置く
+    mv dotfiles-main $DOTPATH
+
+else
+    exit "curl or wget required"
+fi
+
+cd $HOME/$DOTPATH
+
+# 移動できたらリンクを実行する
+for file in .*
+do 
+	["$file" = ".git"] && continue
+	
+	ln -snvf "$DOTPATH/$file" "$HOME"/"$file" 
+done
--- install.sh
+++ install.sh
@@ -1,37 +1,41 @@
-if [ ! -e ~/dotfiles ]; then
-	mkdir ~/dotfiles
+DOTPATH=~/github/dotfiles
+
+if [ ! -e "$DOTPATH" ]; then
+	mkdir "$DOTPATH"
 fi
 
-DOTPATH="dotfiles"
-
-if type git > /dev/null 2>&1; then
-	git clone --recursive "https://github.com/dghnts/dotfiles.git" "$DOTPATH"
-elif type curl > /dev/null 2>&1 || type wget > /dev/null 2>&1; then
-
-    tarball="https://github.com/dghnts/dotfiles/archive/main.tar.gz"
-
-    # どっちかでダウンロードして，tar に流す
-    if type curl > /dev/null 2>&1; then
-        curl -L "$tarball"
-
-    elif type wget > /dev/null/ 2>&1; then
-        wget -O - "$tarball"
+if [ ! -e "$HOME/github" ]; then
+	if type git > /dev/null 2>&1 ; then
+		git clone --recursive "https://github.com/dghnts/dotfiles.git" "$DOTPATH"
+	elif type curl > /dev/null 2>&1 || type wget > /dev/null 2>&1; then
+		tarball="https://github.com/dghnts/dotfiles/archive/main.tar.gz"
+		
+		# どっちかでダウンロードして，tar に流す
+		if type curl > /dev/null 2>&1; then
+			curl -L "$tarball"
+		
+		elif type wget > /dev/null/ 2>&1; then
+			wget -O - "$tarball"
 	
-    fi | tar zxv
-
-    # 解凍したら，DOTPATH に置く
-    mv dotfiles-main $DOTPATH
-
-else
-    exit "curl or wget required"
+		fi | tar zxv
+		
+		# 解凍したら，DOTPATH に置く
+    		mv dotfiles-main $DOTPATH
+	else
+		exit "curl or wget required"
+	fi
 fi
 
-cd $HOME/$DOTPATH
+cd "$DOTPATH"
 
+echo $PWD
 # 移動できたらリンクを実行する
 for file in .*
 do 
-	["$file" = ".git"] && continue
-	
+	if [ "$file" = ".git" ] || [ "$file" = ".gitignore" ] || [ "$file" = "." ] || [ "$file" = ".." ]; then
+		continue
+	else
+		echo $file
 	ln -snvf "$DOTPATH/$file" "$HOME"/"$file" 
+	fi
 done
